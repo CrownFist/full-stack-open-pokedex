7d298871442c8712862c8dacb378c605c24aedec
crownfist <jekron@gmail.com>
2024-02-11 13:09:26 +0200

    added two more tests to cypress e2e

--- cypress/e2e/pokedex-app.cy.js
+++ cypress/e2e/pokedex-app.cy.js
@@ -6,4 +6,18 @@ describe('Pokedex', function () {
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
   })
+  it('pokemon page can be navigated to', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('arbok').click()
+    cy.contains('unnerve')
+  })
+  it('pokemon page can be navigated to and back to homepage', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('arbok').click()
+    cy.contains('unnerve')
+    cy.contains('Home').click()
+    cy.contains(
+      'Pokémon and Pokémon character names are trademarks of Nintendo.'
+    )
+  })
 })
